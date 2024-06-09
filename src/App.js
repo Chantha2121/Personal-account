e9b19348c72950bc695bc3334a341e38ec3d645b
Eventlistener <chanfcsg334@gmail.com>
2024-06-09 21:41:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar1 from "./components/Navbar1";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar1/>
+      <Hero/>
+      <About/>
+      <Work/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
